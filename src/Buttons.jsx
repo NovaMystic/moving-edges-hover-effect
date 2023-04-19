f2ba9901988a0d909aefbf8d1ef9d1c381fa017e
Mystic <mystic.js899@gmail.com>
2023-04-19 08:04:43 +0300

    first commit

---
+++ src/Buttons.jsx
@@ -0,0 +1,27 @@
+export default function Buttons() {
+  return (
+    <div className="container">
+      <a href="#">
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        Button
+      </a>
+      <a href="#">
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        Button
+      </a>
+      <a href="#">
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        Button
+      </a>
+    </div>
+  );
+}
